73968e0ccfaeff9d439c4fb7f65768e59b60701c
mkondratev <mkondratev@srv.rbt>
2023-06-07 15:14:19 +0300

    second blood

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod controllers;
 use controllers::CatsController;
 
 use shuttle_rocket::ShuttleRocket;
+use shuttle_runtime::CustomError;
 use sqlx::{Executor, PgPool};
 
 
@@ -12,11 +13,11 @@ struct MyState(PgPool);
 
 #[shuttle_runtime::main]
 async fn rocket(#[shuttle_shared_db::Postgres] pool: PgPool) -> ShuttleRocket {
-    pool.execute(include_str!("../schema.sql")).await.unwrap();
+    pool.execute(include_str!("../schema.sql")).await.map_err(CustomError::new)?;
     let state = MyState(pool);
     let rocket = rocket::build()
         .manage(state)
-        .mount("/", CatsController);
+        .mount("/api", CatsController);
     Ok(rocket.into())
 }
 
